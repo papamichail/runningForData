a51baa088a1798e66e442add0924d104cda8b489
papamichail <papamichail@gmail.com>
2020-12-30 01:12:09 +0200

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+library(dplyr)
+
+filename <- "myDataset.zip"
+
+# Checking if the file already exists online.
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename)
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
